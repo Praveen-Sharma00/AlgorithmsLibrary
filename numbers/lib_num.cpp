955739b04cff9be4c0317c1bcecd80b6beffec6a
Praveen Sharma <praveensharma.inbox@gmail.com>
2019-10-07 18:17:10 +0530

    Fixed a bug related to build

--- numbers/lib_num.cpp
+++ numbers/lib_num.cpp
@@ -2,10 +2,10 @@
 #include "lib_num.hpp"
 
 void sieve(int n){
-    VectorBool primes(n+1);
+    VectorBool primes(n+1,true);
     for(int p=2;p*p<=n;p++){
-        if(primes.at(p)==true){
-            for(int i=p*p;i<=n;i++){
+        if(primes[p]==true){
+            for(int i=p*p;i<=n;i+=p){
                 primes[i]=false;
             }
         }
@@ -13,4 +13,5 @@ void sieve(int n){
     for (int p=2; p<=n; p++) 
         if (primes[p]) 
           cout << p << " "; 
+    
 }
